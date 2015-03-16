66b1847caa332e69aeb057dcc821181fff2454d5
unknown <qinyangzhang@021ZJ1786.synacast.local>
2015-03-16 10:04:37 +0800

    '添加信息屏蔽'

--- manage.sample.com/index.php
+++ manage.sample.com/index.php
@@ -1,4 +1,5 @@
 <?php
+error_reporting(E_ALL &~E_NOTICE &~E_STRICT);
 // Define path to application directory
 defined('APPLICATION_PATH')
     || define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/application'));
